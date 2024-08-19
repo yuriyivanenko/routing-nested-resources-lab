83972ed7e0948b273a5033b2b7ce422c34004baa
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-08-19 14:59:04 -0400

    Pass all tests

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,27 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      artist = Artist.find_by(id: params[:artist_id])
+      if artist
+        @songs = artist.songs
+      else
+        flash[:alert] = "Artist not found"
+        redirect_to artists_path
+      end
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
-  end
+    @song = Song.find_by(id: params[:id])
+    if @song
+      @song
+    else
+      flash[:alert] = "Song not found"  # Setting the flash message as the test expects
+      redirect_to artist_songs_path  # Assuming @artist is set correctly
+    end
+end
 
   def new
     @song = Song.new
